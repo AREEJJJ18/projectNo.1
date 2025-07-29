bd96ac7a8e1d8599005021cdc8fcabdbd903acb9
AREEJJJ18 <areejnasir281@gmail.com>
2025-07-29 15:36:55 +0500

    added request status in errorResponse function

--- Utilities/errorResponseHandling.js
+++ Utilities/errorResponseHandling.js
@@ -1,6 +1,7 @@
 function errorResponse(res, statusCode, message)
 {
      return res.status(statusCode).json({
+        request_status : "failed",
         errors: Array.isArray(message) ? message : [message],
     });
 }
